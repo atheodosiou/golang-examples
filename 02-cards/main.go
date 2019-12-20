52416945bd04012b7794b147c3267e23f161b68c
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-12-20 13:58:58 +0200

    Slices and for loops

--- 02-cards/main.go
+++ 02-cards/main.go
@@ -6,8 +6,19 @@ func main() {
 	//VARIABLE DECLARATIONS
 	//var card string ="Ace of Spades"
 	// card := "Ace of Spades"
-	card := newCard()
-	fmt.Println(card)
+
+	// card := newCard()
+	// fmt.Println(card)
+
+	//Slice Declaration
+	cards := []string{"Ace of Spades", newCard()}
+	//Append new item on a slice
+	cards = append(cards, "Six of Spades")
+
+	//Loops
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
