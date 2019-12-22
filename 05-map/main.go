904451403dd5a7f644c0b50fb9da9601f04d6717
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-12-22 22:41:18 +0200

    Update maps

--- 05-map/main.go
+++ 05-map/main.go
@@ -7,20 +7,29 @@ func main() {
 
 	//First way
 
-	// colors := map[string]string{
-	// 	"red":   "#ff000",
-	// 	"green": "4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
+	printMap(colors)
 	//Second way
 	// var colors map[string]string
 
 	//Third way
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff"
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
 
 	// Deleting a key value pair from a map
-	delete(colors, "white")
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+}
+
+//Iterate over a map
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
