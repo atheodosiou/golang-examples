9839c60062c219e9466dbecd45bbd70b966b62e8
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-12-22 22:07:58 +0200

    Rich maps

--- 05-map/main.go
+++ 05-map/main.go
@@ -18,5 +18,9 @@ func main() {
 	//Third way
 	colors := make(map[string]string)
 	colors["white"] = "#ffffff"
+
+	// Deleting a key value pair from a map
+	delete(colors, "white")
+
 	fmt.Println(colors)
 }
